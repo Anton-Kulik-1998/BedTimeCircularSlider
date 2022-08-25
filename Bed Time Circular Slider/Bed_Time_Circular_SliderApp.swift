88c9399f6fe27e8a4524f387c76d99f921751db2
Антон Кулик <anton.smile.1998@gmail.com>
2022-08-25 21:33:58 +0300

    Initial Commit

---
+++ Bed Time Circular Slider/Bed_Time_Circular_SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bed_Time_Circular_SliderApp.swift
+//  Bed Time Circular Slider
+//
+//  Created by Антон Кулик on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Bed_Time_Circular_SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bed Time Circular Slider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bed Time Circular Slider
+//
+//  Created by Антон Кулик on 25.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
